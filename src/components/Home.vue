e55d014c97052ea0c0b4e4095f77430a96d7fd62
anigels <a.nigels.scs@gmail.com>
2026-08-31 10:13:25 -0400

    Update Home.vue text fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -301,7 +301,7 @@ export default {
       }
 
       if (!this.courtCount || this.courtCount < 1) {
-        this.showMessage('Please enter a valid number courts.');
+        this.showMessage('Please enter a valid number of courts.');
         return;
       }
 
