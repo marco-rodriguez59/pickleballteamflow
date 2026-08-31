9dabd8d2e621b4d53671547d0f55fdc0bd59427b
anigels <a.nigels.scs@gmail.com>
2026-08-31 11:08:01 -0400

    Update vite.config.js for app version

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import packageJson from './package.json' with { type: 'json' }
 
 export default defineConfig({
   plugins: [vue()],
   base: './',
+
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version)
+  },
+
   build: {
     outDir: 'www',
     emptyOutDir: false,
