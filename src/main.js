12ca683c572772c92ee05818e0f4cf50b9fc38e3
anigels <a.nigels.scs@gmail.com>
2026-08-31 17:02:34 -0400

    Update main.js to use ionic UI

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { IonicVue } from '@ionic/vue'
+
+import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/float-elements.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/display.css'
+
 import App from './App.vue'
 import Home from './components/Home.vue'
 import About from './components/About.vue'
@@ -18,5 +31,10 @@ const router = createRouter({
 
 // Create and mount app
 const app = createApp(App)
+
+app.use(IonicVue)
 app.use(router)
-app.mount('#app')
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
