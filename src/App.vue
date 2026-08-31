7e3c272abf8ae08d7db169aca3b81826735243c2
anigels <a.nigels.scs@gmail.com>
2026-08-31 10:20:32 -0400

    Update App.vue to remove dice icon

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4 app-navbar">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">🎲 Pickleball Team Flow</a>
+        <a class="navbar-brand" href="#">🏓 Pickleball Team Flow</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
